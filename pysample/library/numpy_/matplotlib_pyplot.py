bbd481d7e00a95020f569e801451d7563404908c
youkidkk <youkidkk80@gmail.com>
2019-12-30 21:35:09 +0900

    numpy のサンプルを更新

---
+++ pysample/library/numpy_/matplotlib_pyplot.py
@@ -0,0 +1,17 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, math.pi * 2, 0.1)
+siny = np.sin(x)
+cosy = np.cos(x)
+
+plt.plot(x, siny, label=" sin")
+plt.plot(x, cosy, label=" cos")
+
+plt.xlabel(" x")
+plt.ylabel(" y")
+plt.title(' sin, cos')
+plt.legend()
+
+plt.show()
